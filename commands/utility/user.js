44b4b508453a734919d9b119e8eaba5475703a1f
Simon <yash1441@yahoo.com>
2024-03-19 15:15:58 +0530

    Made the command server only

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -4,7 +4,8 @@ module.exports = {
 	category: 'utility',
 	data: new SlashCommandBuilder()
 		.setName('user')
-		.setDescription('Provides information about the user.'),
+		.setDescription('Provides information about the user.')
+		.setDMPermission(false),
 	async execute(interaction) {
 		await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
 	},
