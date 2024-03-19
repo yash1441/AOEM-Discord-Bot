1a8acb7dc51b313f81dc734d05078713116d27d9
Simon <yash1441@yahoo.com>
2024-03-19 15:16:05 +0530

    Added suggestion command

---
+++ commands/server/suggestion.js
@@ -0,0 +1,42 @@
+const { SlashCommandBuilder, EmbedBuilder, messageLink } = require('discord.js');
+
+module.exports = {
+    cooldown: 60,
+    category: 'server',
+    data: new SlashCommandBuilder()
+        .setName('suggestion')
+        .setDescription('Submit a suggestion.')
+        .setDMPermission(false)
+        .addStringOption(option =>
+            option.setName('details')
+                .setDescription('Enter the details of the suggestion.')
+                .setRequired(true))
+        .addAttachmentOption(option =>
+            option.setName('attachment')
+                .setDescription('Attach a file to the suggestion.')
+                .setRequired(false)
+        ),
+    async execute(interaction) {
+        await interaction.deferReply({ ephemeral: true });
+
+        const details = await interaction.options.getString('details');
+        const attachment = await interaction.options.getAttachment('attachment');
+
+        const embed = new EmbedBuilder()
+            .setColor('White')
+            .setAuthor({
+                name: interaction.user.tag,
+                iconURL: interaction.user.displayAvatarURL({ dynamic: true })
+            })
+            .setDescription(details)
+            .setTimestamp();
+
+        if (attachment) embed.setImage(attachment.url);
+
+        const suggestion = await interaction.channel.send({ embeds: [embed] });
+
+        await suggestion.react('✅').then(suggestion.react('❌'));
+
+        await interaction.editReply({ content: 'Suggestion Submitted! ' + messageLink(suggestion.channelId, suggestion.id) });
+    },
+};
