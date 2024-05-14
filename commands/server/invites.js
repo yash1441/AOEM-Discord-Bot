51fb1614c796bcd80895754f74d874debbd92017
Simon <yash1441@yahoo.com>
2024-05-14 14:32:49 +0530

    Added total invites to leaderboard

--- commands/server/invites.js
+++ commands/server/invites.js
@@ -1,13 +1,13 @@
 const { SlashCommandBuilder, PermissionFlagsBits, userMention, codeBlock } = require('discord.js');
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize({
+const sequelize1 = new Sequelize({
     dialect: 'sqlite',
     storage: 'db/invites.sqlite',
     logging: false,
 });
 
-const Invites = sequelize.define('invites', {
+const Invites = sequelize1.define('invites', {
     code: {
         type: Sequelize.STRING,
         allowNull: false,
@@ -21,6 +21,33 @@ const Invites = sequelize.define('invites', {
         type: Sequelize.INTEGER,
         allowNull: false,
         defaultValue: 0,
+    },
+    guild_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: process.env.GUILD_ID,
+    },
+});
+
+const sequelize2 = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'db/total_invites.sqlite',
+    logging: false,
+});
+
+const TotalInvites = sequelize2.define('total_invites', {
+    code: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    user_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    uses: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
     },
     guild_id: {
         type: Sequelize.STRING,
@@ -48,9 +75,10 @@ module.exports = {
         ),
     async execute(interaction) {
         const subCommand = interaction.options.getSubcommand();
+        await Invites.sync();
+        await TotalInvites.sync();
         if (subCommand === 'reset') {
             if (!interaction.member.permissions.has(PermissionFlagsBits.Administrator)) return await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
-            await Invites.sync();
             await Invites.update({ 'uses': 0 }, { where: {} });
             return await interaction.reply({ content: 'Invites leaderboard has been reset.', ephemeral: true });
         } else if (subCommand === 'leaderboard') {
@@ -58,20 +86,33 @@ module.exports = {
             const invites = await Invites.findAll({
                 attributes: ['user_id', 'uses'],
             });
-            const finalInvites = [];
-            for (const invite of invites) {
-                const { user_id, uses } = invite;
-                const existingInvite = finalInvites.find(invite => invite.user_id === user_id);
-                (existingInvite) ? existingInvite.uses += uses : finalInvites.push({ user_id, uses });
-            }
-            finalInvites.sort((a, b) => b.uses - a.uses);
-            const top10 = finalInvites.slice(0, 10);
-            let message = ''
-            for (const invite of top10) {
+            const TotalInvites = await Invites.findAll({
+                attributes: ['user_id', 'uses'],
+            });
+            const topWeekly = getTop10Invites(invites);
+            const top = getTop10Invites(TotalInvites);
+            let message = '**Top Weekly**'
+            for (const invite of topWeekly) {
                 const user = userMention(invite.user_id);
                 message += `${user}: ${invite.uses}\n`;
             }
-            await interaction.editReply({ content: 'Invites leaderboard for this week:\n' + message });
+            message += '\n\n**Top Alltime**';
+            for (const invite of top) {
+                const user = userMention(invite.user_id);
+                message += `${user}: ${invite.uses}\n`;
+            }
+            await interaction.editReply({ content: '## Invites Leaderboard\n' + message });
         }
     },
 };
+
+function getTop10Invites(invites) {
+    const finalInvites = [];
+    for (const invite of invites) {
+        const { user_id, uses } = invite;
+        const existingInvite = finalInvites.find(invite => invite.user_id === user_id);
+        (existingInvite) ? existingInvite.uses += uses : finalInvites.push({ user_id, uses });
+    }
+    finalInvites.sort((a, b) => b.uses - a.uses);
+    return finalInvites.slice(0, 10);
+}
