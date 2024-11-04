db3ba86157d4153d3e5fb379c4903817e26d321d
Simon <yash1441@yahoo.com>
2024-11-04 23:40:45 +0530

    Formatting

--- components/buttons/kr-alliance-register.js
+++ components/buttons/kr-alliance-register.js
@@ -1,50 +1,64 @@
-const { ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
-require('dotenv').config();
+const {
+	ModalBuilder,
+	ActionRowBuilder,
+	TextInputBuilder,
+	TextInputStyle,
+} = require("discord.js");
+require("dotenv").config();
 
 module.exports = {
-    cooldown: 10,
-    data: {
-        name: 'kr-alliance-register',
-    },
-    async execute(interaction) {
-        const modal = new ModalBuilder()
-            .setCustomId('kr-alliance-register-modal')
-            .setTitle('KR Alliance Registration');
+	cooldown: 10,
+	data: {
+		name: "kr-alliance-register",
+	},
+	async execute(interaction) {
+		const modal = new ModalBuilder()
+			.setCustomId("kr-alliance-register-modal")
+			.setTitle("KR Alliance Registration");
 
-        const server = new TextInputBuilder()
-            .setCustomId('server')
-            .setLabel('Server')
-            .setStyle(TextInputStyle.Short)
-            .setRequired(true);
+		const server = new TextInputBuilder()
+			.setCustomId("server")
+			.setLabel("Server")
+			.setStyle(TextInputStyle.Short)
+			.setRequired(true);
 
-        const allianceName = new TextInputBuilder()
-            .setCustomId('allianceName')
-            .setLabel('Alliance Name')
-            .setStyle(TextInputStyle.Short)
-            .setRequired(true);
+		const allianceName = new TextInputBuilder()
+			.setCustomId("allianceName")
+			.setLabel("Alliance Name")
+			.setStyle(TextInputStyle.Short)
+			.setRequired(true);
 
-        const comment = new TextInputBuilder()
-            .setCustomId('comment')
-            .setLabel('Comment')
-            .setStyle(TextInputStyle.Paragraph)
-            .setRequired(false);
+		const comment = new TextInputBuilder()
+			.setCustomId("comment")
+			.setLabel("Comment")
+			.setStyle(TextInputStyle.Paragraph)
+			.setRequired(false);
 
-        const firstActionRow = new ActionRowBuilder().addComponents(server);
-        const secondActionRow = new ActionRowBuilder().addComponents(allianceName);
-        const thirdActionRow = new ActionRowBuilder().addComponents(comment);
+		const firstActionRow = new ActionRowBuilder().addComponents(server);
+		const secondActionRow = new ActionRowBuilder().addComponents(
+			allianceName
+		);
+		const thirdActionRow = new ActionRowBuilder().addComponents(comment);
 
-        modal.addComponents(firstActionRow, secondActionRow, thirdActionRow);
+		modal.addComponents(firstActionRow, secondActionRow, thirdActionRow);
 
-        await interaction.showModal(modal);
+		await interaction.showModal(modal);
 
-        interaction.awaitModalSubmit({ time: 300_000 })
-            .then((modalInteraction) => {
-                const server = modalInteraction.fields.getTextInputValue('server');
-                const allianceName = modalInteraction.fields.getTextInputValue('allianceName');
-                const comment = modalInteraction.fields.getTextInputValue('comment');
+		interaction
+			.awaitModalSubmit({ time: 300_000 })
+			.then((modalInteraction) => {
+				const server =
+					modalInteraction.fields.getTextInputValue("server");
+				const allianceName =
+					modalInteraction.fields.getTextInputValue("allianceName");
+				const comment =
+					modalInteraction.fields.getTextInputValue("comment");
 
-                modalInteraction.reply({ content: `Server: ${server}\nAlliance Name: ${allianceName}\nComment: ${comment}` });
-            })
-            .catch(console.error);
-    },
+				modalInteraction.reply({
+					content: `Server: ${server}\nAlliance Name: ${allianceName}\nComment: ${comment}`,
+					ephemeral: true,
+				});
+			})
+			.catch(console.error);
+	},
 };
