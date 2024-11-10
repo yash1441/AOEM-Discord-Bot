a0fe7fb73a7b2dc0cd42e47517b153cc389a0e58
Simon <yash1441@yahoo.com>
2024-11-11 03:40:34 +0530

    Button check

---
+++ components/buttons/check-alliance.js
@@ -0,0 +1,72 @@
+const { EmbedBuilder, bold } = require("discord.js");
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize({
+	dialect: "sqlite",
+	storage: "db/alliance.sqlite",
+	logging: true,
+});
+
+const Alliance = sequelize.define(
+	"jp_alliance",
+	{
+		user_id: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			unique: false,
+		},
+		user_name: {
+			type: Sequelize.STRING,
+			allowNull: false,
+		},
+		server: {
+			type: Sequelize.STRING,
+			allowNull: false,
+		},
+		alliance_name: {
+			type: Sequelize.STRING,
+			allowNull: false,
+		},
+		comment: {
+			type: Sequelize.TEXT,
+			allowNull: true,
+		},
+		createdAt: {
+			type: Sequelize.DATEONLY,
+			allowNull: false,
+			defaultValue: Sequelize.DataTypes.NOW, // This ensures a timestamp is set upon creation
+		},
+	},
+	{ timestamps: false }
+);
+
+module.exports = {
+	cooldown: 10,
+	data: {
+		name: "check-alliance",
+	},
+	async execute(interaction) {
+		await interaction.deferReply({ ephemeral: true });
+		Alliance.sync();
+
+		const currentMonthStart = new Date();
+		currentMonthStart.setDate(1);
+		currentMonthStart.setHours(0, 0, 0, 0);
+		const nextMonthStart = new Date(currentMonthStart);
+		nextMonthStart.setMonth(nextMonthStart.getMonth() + 1);
+
+		const records = await Alliance.findAll({
+			where: {
+				createdAt: {
+					[Sequelize.Op.gte]: currentMonthStart,
+					[Sequelize.Op.lt]: nextMonthStart,
+				},
+			},
+		});
+
+		console.log(records);
+
+		await interaction.reply({ content: "Test" });
+	},
+};
