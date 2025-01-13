45be73fea15216f901d8dd2fdf420e3f34cea2e8
yash-bst <yash1441@yahoo.com>
2025-01-13 15:10:39 +0700

    Create check-pioneer.js

---
+++ components/buttons/check-pioneer.js
@@ -0,0 +1,37 @@
+const { EmbedBuilder, MessageFlags } = require("discord.js");
+const Sheets = require("../../utils/sheets");
+require("dotenv").config();
+
+module.exports = {
+    cooldown: 10,
+    data: {
+        name: "check-pioneer",
+    },
+    async execute(interaction) {
+        await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+
+        const record = await Sheets.findRow(
+            process.env.PIONEER_REGISTRATION_SHEET,
+            "Registration!A2:Z",
+            interaction.user.id
+        );
+
+        const embed = new EmbedBuilder()
+            .setTitle("Pioneer Registration")
+            .setColor("White")
+            .setFooter({
+                text: "Age of Empires Mobile",
+                iconURL: "https://i.ibb.co/Fm4fttV/Logo.png",
+            })
+            .addFields({
+                name: "Discord ID",
+                value: interaction.user.id,
+            });
+
+        console.log(record.values);
+
+        await interaction.editReply({
+            embeds: [embed],
+        });
+    },
+};
