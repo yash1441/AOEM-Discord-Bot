608c1fb3d65d054a26060efd813f41cfe3ea516c
Simon <yash1441@yahoo.com>
2025-01-26 01:17:30 +0530

    Updated registration stuff

--- commands/server/register.js
+++ commands/server/register.js
@@ -1,59 +1,77 @@
 const {
-    SlashCommandBuilder,
-    EmbedBuilder,
-    ButtonBuilder,
-    ButtonStyle,
-    ActionRowBuilder,
-    PermissionFlagsBits,
-    MessageFlags,
+	SlashCommandBuilder,
+	EmbedBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	ActionRowBuilder,
+	PermissionFlagsBits,
+	MessageFlags,
 } = require("discord.js");
 
 module.exports = {
-    cooldown: 60,
-    category: "server",
-    data: new SlashCommandBuilder()
-        .setName("register")
-        .setDescription("Registration related commands")
-        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-        .setDMPermission(false)
-        .addSubcommand((subcommand) =>
-            subcommand
-                .setName("setup")
-                .setDescription("Setup register embed and buttons")
-        ),
-    async execute(interaction) {
-        await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+	cooldown: 60,
+	category: "server",
+	data: new SlashCommandBuilder()
+		.setName("register")
+		.setDescription("Registration related commands")
+		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+		.setDMPermission(false)
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName("setup")
+				.setDescription("Setup register embed and buttons")
+		),
+	async execute(interaction) {
+		await interaction.deferReply({ flags: MessageFlags.Ephemeral });
 
-        const embed = new EmbedBuilder()
-            .setTitle("Pioneer Server Registration")
-            .setColor("White")
-            .setDescription("Register for Pioneer server.")
-            .setFooter({
-                text: "Age of Empires Mobile",
-                iconURL: "https://i.ibb.co/Fm4fttV/Logo.png",
-            });
+		const embed = new EmbedBuilder()
+			.setTitle("1.4 Version Pioneer Server Recruitment is now open!")
+			.setColor("White")
+			.setDescription("Register for Pioneer server.")
+			.addFields(
+				{
+					name: "Registration Period: 2.2 - 2.4, 23:59 (UTC)",
+					value: "Click the sign-up button to fill in the necessary information and complete your registration. Please note that each Discord account can only register once, so please fill in your details carefully.",
+					inline: false,
+				},
+				{
+					name: "Qualification Distribution: 2.5 - 2.7",
+					value: 'During the qualification distribution period, click on the "Check" button to view your registration results. If you are selected, the Bot will send you a CDK to join the Pioneer Server.',
+					inline: false,
+				},
+				{
+					name: "Pioneer Server Testing Period: 2.7 - 2.16",
+					value: "\u200b",
+					inline: false,
+				}
+			)
+			.setImage("https://i.ibb.co/LQr54Zy/image.png")
+			.setFooter({
+				text: "Age of Empires Mobile",
+				iconURL: "https://i.ibb.co/Fm4fttV/Logo.png",
+			});
 
-        const registerButton = new ButtonBuilder()
-            .setLabel("Register")
-            .setStyle(ButtonStyle.Primary)
-            .setCustomId("register-pioneer")
-            .setEmoji("📃");
+		const registerButton = new ButtonBuilder()
+			.setLabel("Sign Up")
+			.setStyle(ButtonStyle.Primary)
+			.setCustomId("register-pioneer")
+			.setEmoji("📃");
 
-        const checkButton = new ButtonBuilder()
-            .setLabel("Check")
-            .setStyle(ButtonStyle.Success)
-            .setCustomId("check-pioneer")
-            .setEmoji("🔍");
+		// const checkButton = new ButtonBuilder()
+		// 	.setLabel("Check")
+		// 	.setStyle(ButtonStyle.Success)
+		// 	.setCustomId("check-pioneer")
+		// 	.setEmoji("🔍");
 
-        const row = new ActionRowBuilder().addComponents(
-            registerButton,
-            checkButton
-        );
+		const row = new ActionRowBuilder().addComponents(
+			registerButton
+			//checkButton
+		);
 
-        await interaction.channel.send({ embeds: [embed], components: [row] });
+		await interaction.channel.send({ embeds: [embed], components: [row] });
 
-        await interaction.editReply({
-            content: "Registration setup complete!",
-        });
-    },
+		await interaction.editReply({
+			content: "Registration setup complete!",
+		});
+	},
 };
