b1821ce3cebd64757af064dec602f29cc26ae6e2
Simon <yash1441@yahoo.com>
2025-12-11 13:10:55 +0530

    Adds join giveaway button functionality
    Implements the "joinGiveaway" button, allowing users to participate in a giveaway.
    
    The button checks if the user has already entered. If not, it instructs them to use a specific phrase in a designated channel to enter.

---
+++ components/buttons/joinGiveaway.js
@@ -0,0 +1,32 @@
+const { MessageFlags, codeBlock, channelMention } = require("discord.js");
+const Sheets = require("../../utils/sheets");
+require("dotenv").config();
+
+module.exports = {
+	cooldown: 10,
+	data: {
+		name: "joinGiveaway",
+	},
+	async execute(interaction) {
+		await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+
+		const record = await Sheets.findRow(
+			process.env.FEEDBACK_SHEET,
+			"Giveaway!A2:Z",
+			interaction.user.id
+		);
+
+		if (record != null)
+			return await interaction.editReply({
+				content: "You have already entered the giveaway.",
+			});
+		else
+			return await interaction.editReply({
+				content:
+					"Please use the phrase below in " +
+					channelMention("1303057362780815472") +
+					" to join the giveaway:\n" +
+					codeBlock("AOEM is the best game"),
+			});
+	},
+};
