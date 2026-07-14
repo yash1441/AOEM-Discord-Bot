56e8259dc446b7679296a75bb9cb0b6a8993964f
Simon <yash1441@yahoo.com>
2026-07-14 16:35:24 +0530

    Update Pioneer client to T11 test build
    The download link for the Pioneer client command now points to version v1.13.20.2, reflecting the latest test build (T11).

--- components/buttons/check-pioneer.js
+++ components/buttons/check-pioneer.js
@@ -11,7 +11,7 @@ module.exports = {
 		await interaction.deferReply({ flags: MessageFlags.Ephemeral });
 
 		const gameLink =
-			"https://download.aoemobile.com/apk/AOEM_And_D_v1.12.105.2_AOEM_T10_Pioneer.apk";
+			"https://download.aoemobile.com/apk/AOEM_And_D_v1.13.20.2_AOEM_T11_Pioneer.apk";
 
 		const record = await Sheets.findRow(
 			process.env.PIONEER_REGISTRATION_SHEET,
